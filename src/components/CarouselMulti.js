f166df631100629e240baa66be7eec1698d8c0ad
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2023-01-23 09:59:00 -0300

    home completa

--- src/components/CarouselMulti.js
+++ src/components/CarouselMulti.js
@@ -20,21 +20,55 @@ export default function CarouselMulti(props) {
     <Carousel
       layout="default"
       data={data}
-      renderItem={item => {
-        <RenderItem data={item} navigation={navigation} />;
-      }}
+      renderItem={item => <RenderItem data={item} navigation={navigation} />}
       sliderWidth={width}
       itemWidth={ITEM_WIDTH}
+      firstItem={1}
+      inactiveSlideScale={1}
+      inactiveSlideOpacity={1}
     />
   );
 }
 
-function RenderItem() {
+function RenderItem(props) {
+  const {data, navigation} = props;
+  const {id, title, poster_path} = data.item;
+  const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`;
+
+  const onNavigation = () => {
+    navigation.navigate('Movie', {id: id});
+  };
+
   return (
-    <View>
-      <Title>Hola</Title>
-    </View>
+    <TouchableWithoutFeedback onPress={onNavigation}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={{uri: imageUrl}} />
+        <Title style={styles.title} numberOfLines={1}>
+          {title}
+        </Title>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+  },
+  image: {
+    width: '85%',
+    height: 170,
+    borderRadius: 20,
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    fontSize: 16,
+  },
+});
